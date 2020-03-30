1984e5c39ab9fae7514894c1e0a8fb140f15564d
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:50:09 +0200

    Initial commit

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+namespace OOP.2.1___EmployeesV10
+{
+    public class Employee
+    {
+        public string _name;
+        public int _hoursPerWeek;
+
+        public Employee(string Name, int HoursPerWeek)
+        {
+            _name = Name;
+            _hoursPerWeek = HoursPerWeek;
+        }
+
+
+    }
+}
---
+++ ITSupporter.cs
@@ -0,0 +1,49 @@
+﻿namespace EmployeesV10
+{
+    public class ITSupporter : Employee
+    {
+        #region Instance fields
+        private string _name;
+        private int _hoursPerWeek;
+        private string _primaryWorkArea;
+        #endregion
+
+        #region Constructor
+        public ITSupporter(string Name, int HoursPerWeek, string PrimaryWorkArea)
+            : base(Name, HoursPerWeek, PrimaryWorkArea)
+        {
+            _name = Name;
+            _hoursPerWeek = HoursPerWeek;
+            _primaryWorkArea = PrimaryWorkArea;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int HoursPerWeek
+        {
+            get { return _hoursPerWeek; }
+            set { _hoursPerWeek = value; }
+        }
+
+        public string PrimaryWorkArea
+        {
+            get { return _primaryWorkArea; }
+            set { _primaryWorkArea = value; }
+        }
+
+        public string AllInformation
+        {
+            get
+            {
+                return $"IT-Supporter {Name} works {HoursPerWeek} hours/week, mostly with {PrimaryWorkArea}";
+            }
+        } 
+        #endregion
+    }
+}
